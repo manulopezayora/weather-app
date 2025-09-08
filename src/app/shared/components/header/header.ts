b49f7b365e3e15b62a281ad9c67e1f5e3041b1b9
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-08 19:13:18 +0200

    chore(header): show links and avatar when user is logged

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { Icon } from '@components/icon/icon';
+import { Store } from '@ngrx/store';
 import { AvatarModule } from 'primeng/avatar';
 import { Toolbar } from 'primeng/toolbar';
+import { selectUser } from 'src/app/store/user/user.selectors';
 
 @Component({
   selector: 'app-header',
-  imports: [Toolbar, AvatarModule, Icon],
+  imports: [Toolbar, AvatarModule, Icon, AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+
+  private store = inject(Store);
+
+  public user$ = this.store.select(selectUser);
 
 }
