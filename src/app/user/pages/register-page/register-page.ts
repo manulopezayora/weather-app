e3d7d6b910b11879e7852ceedfbfde1e3fd3340a
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-08 13:33:09 +0200

    chore(register-page): add title

--- src/app/user/pages/register-page/register-page.ts
+++ src/app/user/pages/register-page/register-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonLink } from '@components/index';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [
+    ButtonLink,
+    RouterLink,
+  ],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css'
 })
