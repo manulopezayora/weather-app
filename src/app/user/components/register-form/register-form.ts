4bd00fd329baf80ad2ea0ab62d3beacb8d1fe1b8
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-08 13:53:30 +0200

    chore(register-page): add register form

--- src/app/user/components/register-form/register-form.ts
+++ src/app/user/components/register-form/register-form.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Button, InputPassword, InputText } from "@components/index";
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [
+    Button,
+    InputPassword,
+    InputText,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css'
 })
 export class RegisterForm {
+
+  private formBuilder = inject(FormBuilder);
+
+  public registerForm: FormGroup = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    confirmPassword: ['', [Validators.required]],
+  });
+
+  public onSubmit() {
+    console.log(this.registerForm.value);
+  }
 
 }
--- src/app/user/pages/register-page/register-page.ts
+++ src/app/user/pages/register-page/register-page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonLink } from '@components/index';
+import { RegisterForm } from '../../components/register-form/register-form';
 
 @Component({
   selector: 'app-register-page',
   imports: [
     ButtonLink,
     RouterLink,
-  ],
+    RegisterForm
+],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css'
 })
