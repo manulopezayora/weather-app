e8503b605dc47ee184d9cc61123d37227bb8a31a
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-09 13:30:07 +0200

    chore(environments): configure environments

--- src/app/weather/services/weatherService/weather-service.ts
+++ src/app/weather/services/weatherService/weather-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,13 @@ export class WeatherService {
 
   private http = inject(HttpClient);
 
-  private apiKey: string = '';
   private baseUrl = 'https://api.openweathermap.org/data/2.5';
 
   getWeatherByCity(city: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/weather?q=${city}&appid=${this.apiKey}&units=metric&lang=es`);
+    return this.http.get(`${this.baseUrl}/weather?q=${city}&appid=${environment.weatherKey}&units=metric&lang=es`);
   }
 
   getForecastByCity(city: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/forecast?q=${city}&appid=${this.apiKey}&units=metric&lang=es`);
+    return this.http.get(`${this.baseUrl}/forecast?q=${city}&appid=${environment.weatherKey}&units=metric&lang=es`);
   }
 }
