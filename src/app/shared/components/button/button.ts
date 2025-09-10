b843505c26a0f1fe0986871bdac00d26685db31f
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-10 12:41:57 +0200

    chore(button): add input color

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -2,6 +2,7 @@ import { Component, computed, input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 type ButtonType = 'submit' | 'button';
+type ButtonColor = 'primary' | 'secondary' | 'success' | 'info' | 'warn' | 'help' | 'danger' | 'contrast';
 
 @Component({
   selector: 'app-button',
@@ -14,6 +15,7 @@ export class Button {
   public label = input<string>();
   public icon = input<string>();
   public type = input<ButtonType>('button');
+  public color = input<ButtonColor>('primary');
 
   public iconButton = computed(() => this.icon() ? `pi pi-${this.icon()}` : '');
 
