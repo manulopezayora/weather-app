8a91f05d28a5605e82f66d552f396e57a874912d
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-09-10 15:30:42 +0200

    chore(favorites): add favorites page

--- src/app/weather/weather.routes.ts
+++ src/app/weather/weather.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { FavoritesPage } from './pages/favorites-page/favorites-page';
 import { HomePage } from './pages/home-page/home-page';
 
 export const weatherRoutes: Routes = [
@@ -8,6 +9,10 @@ export const weatherRoutes: Routes = [
       {
         path: 'home',
         component: HomePage
+      },
+      {
+        path: 'favorites',
+        component: FavoritesPage
       },
       {
         path: '**',
